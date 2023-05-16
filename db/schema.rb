fe09895160a1be84868649710d55c575ad83c120
larezafw <farhanlareza@gmail.com>
2023-05-16 10:18:27 +0700

    Downraded to ruby 2.5.9

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_15_034020) do
+ActiveRecord::Schema.define(version: 2023_05_15_034020) do
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.boolean "done"
     t.integer "todo_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["todo_id"], name: "index_items_on_todo_id"
   end
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.string "created_by"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "items", "todos"
